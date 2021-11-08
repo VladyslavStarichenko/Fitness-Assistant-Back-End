2da2ded8f2f342c22abfb04979f47c45b0553f7c
goldsilver12 <vladyslav.starichenko@nure.ua>
2021-11-08 22:08:32 +0200

    Cors problem

--- src/main/java/ua/com/nure/fitnessassistant/FitnessAssistantApplication.java
+++ src/main/java/ua/com/nure/fitnessassistant/FitnessAssistantApplication.java
@@ -18,7 +18,7 @@ public class FitnessAssistantApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:8080");
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
 			}
 		};
 	}
