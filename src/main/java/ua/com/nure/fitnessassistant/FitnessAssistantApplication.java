a4ee1b3f7d94d61271d407355cb074693be58437
goldsilver12 <vladyslav.starichenko@nure.ua>
2021-11-08 22:11:36 +0200

    Cors problem

--- src/main/java/ua/com/nure/fitnessassistant/FitnessAssistantApplication.java
+++ src/main/java/ua/com/nure/fitnessassistant/FitnessAssistantApplication.java
@@ -13,13 +13,13 @@ public class FitnessAssistantApplication {
 		SpringApplication.run(FitnessAssistantApplication.class, args);
 	}
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
-			}
-		};
-	}
+//	@Bean
+//	public WebMvcConfigurer corsConfigurer() {
+//		return new WebMvcConfigurer() {
+//			@Override
+//			public void addCorsMappings(CorsRegistry registry) {
+//				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+//			}
+//		};
+//	}
 }
