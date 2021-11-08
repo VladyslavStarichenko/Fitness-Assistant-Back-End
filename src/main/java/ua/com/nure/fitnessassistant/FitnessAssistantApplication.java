d7a13cc2b136e8facdb5fec5947477bd9fdf9f95
goldsilver12 <vladyslav.starichenko@nure.ua>
2021-11-08 22:53:04 +0200

    Cors problem

--- src/main/java/ua/com/nure/fitnessassistant/FitnessAssistantApplication.java
+++ src/main/java/ua/com/nure/fitnessassistant/FitnessAssistantApplication.java
@@ -12,5 +12,13 @@ public class FitnessAssistantApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FitnessAssistantApplication.class, args);
 	}
-
+//	@Bean
+//	public WebMvcConfigurer corsConfigurer() {
+//		return new WebMvcConfigurer() {
+//			@Override
+//			public void addCorsMappings(CorsRegistry registry) {
+//				registry.addMapping("/").allowedOrigins("http://localhost:8080");
+//			}
+//		};
+//	}
 }
--- src/main/java/ua/com/nure/fitnessassistant/config/SecurityConfig.java
+++ src/main/java/ua/com/nure/fitnessassistant/config/SecurityConfig.java
@@ -13,8 +13,13 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import ua.com.nure.fitnessassistant.security.jwt.JwtConfigurer;
 import ua.com.nure.fitnessassistant.security.jwt.JwtTokenProvider;
+
+import java.util.Arrays;
 
 
 @Configuration
@@ -45,6 +50,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().cors()
                 .and()
                 .authorizeRequests()
                 .antMatchers(LOGIN_ENDPOINT).permitAll()
@@ -56,6 +62,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     }
 
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT", "DELETE"));
+        configuration.setAllowCredentials(true);
+        //the below three lines will add the relevant CORS response headers
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().antMatchers("/v2/api-docs",
