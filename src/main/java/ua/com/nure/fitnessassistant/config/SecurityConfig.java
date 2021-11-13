0234a8b7c65a3d2acecc8ed494bf86d97632ed65
goldsilver12 <vladyslav.starichenko@nure.ua>
2021-11-13 14:38:34 +0200

    Cors problem

--- src/main/java/ua/com/nure/fitnessassistant/config/SecurityConfig.java
+++ src/main/java/ua/com/nure/fitnessassistant/config/SecurityConfig.java
@@ -52,24 +52,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .cors()
-                .and()
-                .csrf()
-                .disable()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                http
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
                 .antMatchers(LOGIN_ENDPOINT).permitAll()
                 .antMatchers(SIGN_UP_ENDPOINT).permitAll()
                 .antMatchers(ADMIN_ENDPOINT).hasRole("ADMIN")
-                .antMatchers("/api/**")
-                .permitAll()
-                .antMatchers(HttpMethod.GET, "/api/**", "/api/**")
-                .permitAll()
-                .anyRequest()
-                .authenticated();
+                .anyRequest().authenticated()
+                .and()
+                .apply(new JwtConfigurer(jwtTokenProvider));
 
 
 
