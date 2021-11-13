0ae2d925543de561003ccb97deb94c05bf3cd698
goldsilver12 <vladyslav.starichenko@nure.ua>
2021-11-13 14:47:57 +0200

    Cors problem

--- src/main/java/ua/com/nure/fitnessassistant/config/SecurityConfig.java
+++ src/main/java/ua/com/nure/fitnessassistant/config/SecurityConfig.java
@@ -52,7 +52,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-                http
+                http.cors()
+                        .and()
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
